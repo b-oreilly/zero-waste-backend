fd0c4ee1877c28c5fabe11ac98e4f4e6aa1e7d15
b-oreilly <berniceoreilly@gmail.com>
2022-04-22 15:59:24 +0100

    .

--- server.js
+++ server.js
@@ -9,15 +9,21 @@ app.use(express.static(path.join(__dirname, "./dist")))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist', 'index.html'))
 })
+
+// Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 3000
 
-const app = express()
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
 
-app.use(cors(corsOptions));
-const corsOptions = {
-    origin: "https://wecycle.netlify.app",
-    credentials: true,
-};
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
 app.use(express.json())
 
